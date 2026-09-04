be6637ce7863caad429aa792474671cc5cd6cacb
Jakub Wilmański <80158921+Wilmanowski@users.noreply.github.com>
2026-09-04 13:00:24 +0200

    feat: add keys in custom input when creating ht

--- src/main/java/org/tkit/onecx/human/task/rs/external/v1/mappers/TasksMapperV1.java
+++ src/main/java/org/tkit/onecx/human/task/rs/external/v1/mappers/TasksMapperV1.java
@@ -1,5 +1,9 @@
 package org.tkit.onecx.human.task.rs.external.v1.mappers;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.tkit.onecx.human.task.domain.models.Task;
@@ -20,7 +24,13 @@ public interface TasksMapperV1 {
     @Mapping(target = "persisted", ignore = true)
     @Mapping(target = "tenantId", ignore = true)
     @Mapping(target = "status", constant = "CREATED")
-    @Mapping(target = "customInput", ignore = true)
     Task toTask(CreateTaskRequestDTOV1 dto);
+
+    default Map<String, String> mapCustomInput(List<String> customInput) {
+        if (customInput == null) {
+            return null;
+        }
+        return customInput.stream().collect(Collectors.toMap(k -> k, k -> "", (a, b) -> a));
+    }
 
 }
